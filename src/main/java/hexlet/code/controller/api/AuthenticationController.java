122c3016aeda959bae8e7c416e114c708352bbec
nastya-ermolaeva <nastyaermolaeva31@mail.ru>
2025-12-14 00:00:24 +0300

    experiment with sentry

--- src/main/java/hexlet/code/controller/api/AuthenticationController.java
+++ src/main/java/hexlet/code/controller/api/AuthenticationController.java
@@ -6,11 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import io.sentry.Sentry;
 
 @RestController
 @RequestMapping("/api")
@@ -30,15 +28,5 @@ public class AuthenticationController {
 
         var token = jwtUtils.generateToken(authRequest.getUsername());
         return token;
-    }
-
-    @GetMapping("/sentry-test")
-    public String testSentry() {
-        try {
-            throw new Exception("Test error for Sentry from java-project-99");
-        } catch (Exception e) {
-            Sentry.captureException(e);
-            return "Test error sent to Sentry! Check your Issues panel.";
-        }
     }
 }
